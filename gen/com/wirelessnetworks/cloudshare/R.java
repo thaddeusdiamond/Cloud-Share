d2952b15292a246cf1f8753e3b5c02b4c0aa01e6
Thaddeus Diamond <thaddeus.diamond@yale.edu>
2011-04-21 22:40:42 -0400

    Finished the home page widget (need to incorporate a new chat with a widget update notification, but the UI is done)

--- gen/com/wirelessnetworks/cloudshare/R.java
+++ gen/com/wirelessnetworks/cloudshare/R.java
@@ -11,49 +11,65 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040003;
-        public static final int blue=0x7f040004;
-        public static final int text_default=0x7f040001;
-        public static final int theme1=0x7f040000;
-        public static final int white=0x7f040002;
+        public static final int black=0x7f050005;
+        public static final int blue=0x7f050006;
+        public static final int text_default=0x7f050003;
+        public static final int theme1=0x7f050000;
+        public static final int theme2=0x7f050001;
+        public static final int theme3=0x7f050002;
+        public static final int white=0x7f050004;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int widget_bg_landscape=0x7f020001;
+        public static final int widget_bg_portrait=0x7f020002;
     }
     public static final class id {
-        public static final int footer=0x7f060000;
-        public static final int frameLayout1=0x7f060001;
-        public static final int imageView1=0x7f060003;
-        public static final int launch_about=0x7f060006;
-        public static final int launch_create=0x7f060005;
-        public static final int launch_search=0x7f060004;
-        public static final int textView1=0x7f060002;
+        public static final int footer=0x7f070000;
+        public static final int frameLayout1=0x7f070003;
+        public static final int imageView1=0x7f070005;
+        public static final int launch_about=0x7f070008;
+        public static final int launch_create=0x7f070007;
+        public static final int launch_search=0x7f070006;
+        public static final int textView1=0x7f070004;
+        public static final int widget_layout=0x7f070001;
+        public static final int widget_main=0x7f070002;
+        public static final int widget_recent1=0x7f070009;
+        public static final int widget_recent2=0x7f07000a;
     }
     public static final class layout {
         public static final int about_us=0x7f030000;
-        public static final int create_network=0x7f030001;
-        public static final int find_network=0x7f030002;
-        public static final int footer=0x7f030003;
-        public static final int loading=0x7f030004;
-        public static final int main=0x7f030005;
+        public static final int cloudshare_widget=0x7f030001;
+        public static final int create_network=0x7f030002;
+        public static final int find_network=0x7f030003;
+        public static final int footer=0x7f030004;
+        public static final int loading=0x7f030005;
+        public static final int main=0x7f030006;
+        public static final int widget_main=0x7f030007;
     }
     public static final class string {
         /**  About Us String Constants 
          */
-        public static final int about_us=0x7f050006;
-        public static final int app_name=0x7f050004;
-        public static final int attribution=0x7f050007;
-        public static final int contact=0x7f050008;
+        public static final int about_us=0x7f060007;
+        public static final int app_name=0x7f060005;
+        public static final int attribution=0x7f060008;
+        public static final int contact=0x7f060009;
         /**  Footer String Constants 
          */
-        public static final int copyright=0x7f050005;
-        public static final int description=0x7f050003;
+        public static final int copyright=0x7f060006;
+        public static final int description=0x7f060004;
+        /**  Widget String Constants 
+         */
+        public static final int empty_widget=0x7f060001;
         /**  Home Page String Constants 
          */
-        public static final int hello=0x7f050001;
+        public static final int hello=0x7f060002;
         /**  Loading Page String Constants 
          */
-        public static final int loading=0x7f050000;
-        public static final int welcome_text=0x7f050002;
+        public static final int loading=0x7f060000;
+        public static final int welcome_text=0x7f060003;
+    }
+    public static final class xml {
+        public static final int cloudshare_widget_info=0x7f040000;
     }
 }

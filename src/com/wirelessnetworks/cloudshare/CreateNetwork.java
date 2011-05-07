3b33889933d1abba37a63f40bab95f4ddf91e7c4
Thaddeus Diamond <thaddeus.diamond@yale.edu>
2011-05-07 12:48:02 -0400

    Minor debugging changes

--- src/com/wirelessnetworks/cloudshare/CreateNetwork.java
+++ src/com/wirelessnetworks/cloudshare/CreateNetwork.java
@@ -159,7 +159,7 @@ public class CreateNetwork extends Activity {
           };
 
         // Register the listener with the Location Manager to receive location updates
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         // ----------------------------------------------------------------------------
 
 		
